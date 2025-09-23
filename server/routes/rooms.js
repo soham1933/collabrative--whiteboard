fea43e1415ce0925a1ac7bbcae98ab5f611be0e9
soham1933 <sohambandbe1832>
2025-09-24 00:46:04 +0530

    121

--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -4,7 +4,7 @@ const Room = require('../models/Room');
 const { nanoid } = require('nanoid');
 
 router.post('/join', async (req, res) => {
-  const { roomId } = req.body;
+  const { roomId, name } = req.body;
   try {
     if (roomId) {
       let room = await Room.findOne({ roomId });
@@ -12,17 +12,18 @@ router.post('/join', async (req, res) => {
         room = new Room({ roomId });
         await room.save();
       }
-      return res.json({ roomId: room.roomId });
+      return res.json({ roomId: room.roomId, name: name || 'Anonymous' });
     }
     const newId = nanoid(6);
     const room = new Room({ roomId: newId });
     await room.save();
-    return res.json({ roomId: room.roomId });
+    return res.json({ roomId: room.roomId, name: name || 'Anonymous' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
   }
 });
+
 
 router.get('/:roomId', async (req, res) => {
   const { roomId } = req.params;
@@ -34,5 +35,7 @@ router.get('/:roomId', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
+console.log("Joining room:", roomId, "for user:", name);
+
 
 module.exports = router;
